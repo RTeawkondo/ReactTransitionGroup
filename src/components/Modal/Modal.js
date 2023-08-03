bf7643b92469b9ea3dbad3d39557c2be80bb1a52
Peto Gurin <no.mi83@yahoo.com.vn>
2023-08-03 14:33:31 +0700

    Some animation

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./Modal.css";
+import { Transition } from "react-transition-group";
+
+const modal = (props) => {
+  const animationTiming = {
+    enter: 400,
+    exit: 1000
+  }
+  return (
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
+  );
+};
+
+export default modal;
